6e26fda156bde987e070c325dc302114b98dd9ed
Emily Resch <eresch527@gmail.com>
2020-05-01 18:53:35 -0500

    public folder

---
+++ public/js/burger.js
@@ -0,0 +1,8 @@
+var orm = require("../../config/orm.js");
+
+$(function() {
+    $(".devour-btn").on("click", function(e){
+        var id = $(this).data("id");
+        var devstatus = $(this).attr();
+    })
+})
